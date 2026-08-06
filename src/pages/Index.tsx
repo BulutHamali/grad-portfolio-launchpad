96e1bac8fd13d8dc63af9e0e058187fb09750aa6
Bulut Hamali <buluthamali@gmail.com>
2026-08-05 22:42:17 -0400

    Move featured projects higher on homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,9 +13,9 @@ const Index = () => {
       <SEO />
       <Hero />
       <VideoIntro />
+      <Projects />
       <About />
       <Skills />
-      <Projects />
       <Contact />
       <Footer />
     </div>
